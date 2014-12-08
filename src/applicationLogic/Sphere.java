035689991afff5b2f1dbf6399da1e3aa7f03b430
s55680 <s55680@beuth-hochschule.de>
2014-12-08 18:04:52 +0100

    message

--- src/applicationLogic/Sphere.java
+++ src/applicationLogic/Sphere.java
@@ -29,7 +29,30 @@ public class Sphere extends Geometry{
      */
     @Override
     public Hit hit(Ray ray) {
-        return null;
+        final double resultA = ray.d.dot(ray.d);
+        if(resultA == 0){
+            return null;
+        }
+        final double resultB = ray.o.sub(this.c).mul(2).dot(ray.d);
+        final double resultC = ray.o.sub(this.c).dot(ray.o.sub(this.c)) - this.r * this.r;
+        final double zaehlerRoot = Math.sqrt(resultB * resultB  - 4 * resultA * resultC);
+        if(zaehlerRoot < 0){
+            return null;
+        }
+        final double resultZaehler1 = resultB * (-1) + zaehlerRoot;
+        final double resultZaehler2 = resultB * (-1) - zaehlerRoot;
+        final double resultNenner = 2 * resultA;
+        final double resultT1 = resultZaehler1 / resultNenner;
+        final double resultT2 = resultZaehler2 / resultNenner;
+        if(resultT1 <= resultT2 && resultT1 > 0){
+            final Hit resultHit = new Hit(resultT1, ray, this);
+            return resultHit;
+        }else if(resultT2 < resultT1 && resultT2 > 0){
+            final Hit resultHit = new Hit(resultT2, ray, this);
+            return resultHit;
+        }else{
+            return null;
+        }
     }
     
 }
